fe49c5a27dded3af5991e1954bb6d825803a9dae
Anuj Singh <anuj.as828@gmail.com>
2025-02-01 13:54:38 +0530

    OAuth login enabled

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,11 @@ import "./App.css";
 import Login from "./views/Login";
 import Dashboard from "./views/Dashboard";
 import { Route, RouterProvider, createBrowserRouter } from "react-router-dom";
-import Signup from "./views/Signup";
 
 const routes = createBrowserRouter([
   {
     path: "login",
     element: <Login />,
-  },
-  {
-    path: "signup",
-    element: <Signup />,
   },
   {
     path: "dashboard",
