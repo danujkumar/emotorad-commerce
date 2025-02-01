3e32713f5d15614b8989da850ef9da3dd074abcc
Anuj Singh <anuj.as828@gmail.com>
2025-02-01 12:18:24 +0530

    login done

---
+++ src/components/Piechart.jsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import { PieChart } from '@mui/x-charts/PieChart';
+import { mobileAndDesktopOS, platforms, valueFormatter } from './webUsageStats';
+
+export default function Piechart() {
+  const [itemData, setItemData] = React.useState();
+
+  return (
+    <Stack
+      direction={{ xs: 'column', md: 'row' }}
+      spacing={{ xs: 0, md: 4 }}
+      sx={{ width: '100%' }}
+    >
+      <Box sx={{ flexGrow: 1 }}>
+        <PieChart
+          series={series}
+          width={400}
+          height={300}
+          slotProps={{
+            legend: { hidden: true },
+          }}
+          onItemClick={(event, d) => setItemData(d)}
+        />{' '}
+      </Box>
+    </Stack>
+  );
+}
+
+const series = [
+  {
+    innerRadius: 0,
+    outerRadius: 80,
+    id: 'platform-series',
+    data: platforms,
+    valueFormatter,
+  },
+  {
+    innerRadius: 100,
+    outerRadius: 120,
+    id: 'OS-series',
+    data: mobileAndDesktopOS,
+    valueFormatter,
+  },
+];
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { BarChart } from "@mui/x-charts/BarChart";
+import Card from "../components/Card";
+import Piechart from "../components/Piechart";
+import TopBar from "../components/TopBar";
 
 function Dashboard() {
   return (
     <>
+      <TopBar/>
       <Navbar />
+
+      <Card />
+      <Piechart />
+      <BarChart
+        xAxis={[{ scaleType: "band", data: ["group A", "group B", "group C"] }]}
+        series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+        width={500}
+        height={300}
+      />
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
